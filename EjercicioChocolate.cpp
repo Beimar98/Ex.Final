cecac012024a12756ecd46c5d88df2c9ecc3f2c7
Luis Antonio Molina Yampa <antoniomolina.yampa@gmail.com>
2018-12-13 03:22:04 -0400

    Update EjercicioChocolate.cpp

--- EjercicioChocolate.cpp
+++ EjercicioChocolate.cpp
@@ -9,24 +9,19 @@ int binarySearch(long long int arr[], int l, int r, int x)
    {
         //cout<<arr[mid] <<" - ";
 
-        // If element is smaller than mid, then
-        // it can only be present in left subarray
+       
         if (arr[mid] > x)
-            return binarySearch3(arr, l, mid-1, x);
+            return binarySearch(arr, l, mid-1, x);
 
-        // If the element is present at the middle
-        // itself
+   
         if (arr[mid] < x)
-            return binarySearch3(arr, mid+1, r, x);
+            return binarySearch(arr, mid+1, r, x);
 
         return mid;
 
-        // Else the element can only be present
-        // in right subarray
+      
    }
 
-   // We reach here when element is not
-   // present in array
    return mid;
 }
 
